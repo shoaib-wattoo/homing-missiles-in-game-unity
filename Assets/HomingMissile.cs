79e6912279887e699ed60cc866144e05eea5a58c
DESKTOP-KF35RC4\ASUS <shoaib.wattoo@gmail.com>
2018-09-27 13:33:40 +0500

    first commit

---
+++ Assets/HomingMissile.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomingMissile : MonoBehaviour
+{
+	#region PUBLIC_VARS
+
+	public Transform target;
+	public Rigidbody2D rigidBody;
+	public float angleChangingSpeed;
+	public float movementSpeed;
+	private float angle;
+	private Vector2 lastPosition;
+
+	#endregion
+
+	#region UNITY_CALLBACKS
+
+	void FixedUpdate ()
+	{
+		Vector2 direction = (Vector2)target.position - (Vector2)transform.position;
+		angle = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+		transform.rotation = Quaternion.Euler (0f, 0f, angle);
+		lastPosition = transform.position;
+		if (Vector2.Distance (((Vector2)target.position), rigidBody.position) >= 10.0f)
+		{
+//			Vector2 direction = (Vector2)target.position - rigidBody.position;
+//			direction.Normalize ();
+//			float rotateAmount = Vector3.Cross (direction, transform.up).z;
+//			rigidBody.angularVelocity = -angleChangingSpeed * rotateAmount;
+			rigidBody.velocity = transform.up * movementSpeed;
+		} else
+		{
+			rigidBody.velocity = -1 * transform.up * movementSpeed;
+		}
+	}
+
+	#endregion
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# create-homing-missiles-in-game-unity-tutorial
+The main objective of this post is to give a brief introduction to Homing Missiles in Game (2D). A script for Homing missile is also included and explained in the article.
